df004d179cf25d65e336ed25a08a15233891ed38
KurtCalma <kurtmcalma@gmail.com>
2025-05-12 23:09:07 -0400

    Create GameFrame.java

---
+++ GameFrame.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+	
+	GamePanel panel;
+	
+	GameFrame() {
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack(); /* Causes this Window to be sized to fit 
+		the preferred sizeand layouts of its subcomponents. 
+		The resulting width andheight of the window are automatically enlarged 
+		if either of dimensions is less than the minimum size 
+		as specifiedby the previous call to the setMinimumSize method. */
+		this.setVisible(true);
+		this.setLocationRelativeTo(null); // Sets the frame in the middle of the screen
+	
+	}
+
+}

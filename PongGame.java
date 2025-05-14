ca5cf431ec473919fbd052ffda0ab9fc57db8d3e
KurtCalma <kurtmcalma@gmail.com>
2025-05-14 11:05:13 -0400

    Update PongGame.java

--- PongGame.java
+++ PongGame.java
@@ -7,10 +7,10 @@ public class PongGame {
 
 	public static void main(String[] args) {
 		
-		JOptionPane.showMessageDialog(null, "Greetings! Welcome to the Solo Pong Game program!");
+		JOptionPane.showMessageDialog(null, "Greetings! Welcome to Pong!");
 		JOptionPane.showMessageDialog(null, "The rules of this game are simple: whichever side gets 10 points first, wins!");
-		JOptionPane.showMessageDialog(null, "To access the left paddle, use W for up and S for down. To access the right paddle, use the page up key for up and page down key for down.");
-		JOptionPane.showMessageDialog(null, "With that in mind, have fun with the game!");
+		JOptionPane.showMessageDialog(null, "To access the left paddle (which is what the user is going to be using), use W for up and S for down.");
+		JOptionPane.showMessageDialog(null, "Without further ado, have fun with the game!");
 		
 		GameFrame frame = new GameFrame();
 		

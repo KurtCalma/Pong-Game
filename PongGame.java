af839b3603c208bc13e1a8818a9009d9dfe2177c
KurtCalma <kurtmcalma@gmail.com>
2025-05-22 11:24:31 -0400

    Update PongGame.java

--- PongGame.java
+++ PongGame.java
@@ -4,6 +4,7 @@ public class PongGame {
 	
 	public static void main(String[] args) {
 		
+		// Display the rules and other messages about the game
 		JOptionPane.showMessageDialog(null, "Greetings! Welcome to Pong!", "Instructions", JOptionPane.INFORMATION_MESSAGE);
 		JOptionPane.showMessageDialog(null, 
 				"The rules of this game are simple: If the user scores 10 points first AND does it UNDER 5 MINUTES, they win!"
@@ -13,7 +14,7 @@ public class PongGame {
 		JOptionPane.showMessageDialog(null, "To access the left paddle, use W for up and S for down.", "Instructions", JOptionPane.INFORMATION_MESSAGE);
 		JOptionPane.showMessageDialog(null, "Have fun with the game!", "Plain Message", JOptionPane.PLAIN_MESSAGE);
 		
-		GameFrame frame = new GameFrame();
+		GameFrame frame = new GameFrame(); // Instantiate frame as an object of the GameFrame class
 		
 	}
 

7225ca19754d419546ba9d950df7db870cb11b01
KurtCalma <kurtmcalma@gmail.com>
2025-05-21 22:32:05 -0400

    Update PongGame.java

--- PongGame.java
+++ PongGame.java
@@ -2,15 +2,20 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PongGame {
-
+	
 	public static void main(String[] args) {
 		
-		JOptionPane.showMessageDialog(null, "Greetings! Welcome to Pong!");
-		JOptionPane.showMessageDialog(null, "The rules of this game are simple: whichever side gets 10 points first, wins!");
-		JOptionPane.showMessageDialog(null, "To access the left paddle (which is what the user is going to be using), use W for up and S for down.");
-		JOptionPane.showMessageDialog(null, "Without further ado, have fun with the game!");
+		JOptionPane.showMessageDialog(null, "Greetings! Welcome to Pong!", "Instructions", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, 
+				"The rules of this game are simple: whichever side gets 10 points first , wins!", 
+				"Instructions", 
+				JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "To access the left paddle, use W for up and S for down.", "Instructions", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "Have fun with the game!", "Plain Message", JOptionPane.PLAIN_MESSAGE);
 		
 		GameFrame frame = new GameFrame();
 		
